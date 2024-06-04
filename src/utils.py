392b653a08404838d1fc399b0f7f3a8b051630a3
javidaf <javid.af@outlook.com>
2024-06-04 21:12:25 +0200

    feat: Add function to transform mean image for plotting

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy import signal
+from cellpose import plot
 
 
 def enhanced_mean_image(ops):
-    """ computes enhanced mean image and adds it to ops
+    """computes enhanced mean image and adds it to ops
 
     Median filters ops["meanImg"] with 4*diameter in 2D and subtracts and
     divides by this median-filtered image to return a high-pass filtered
@@ -32,8 +33,13 @@ def enhanced_mean_image(ops):
         ops["spatscale_pix"] = diameter[1]
         ops["aspect"] = diameter[0] / diameter[1]
 
-    diameter = 4 * np.ceil(
-        np.array([ops["spatscale_pix"] * ops["aspect"], ops["spatscale_pix"]])) + 1
+    diameter = (
+        4
+        * np.ceil(
+            np.array([ops["spatscale_pix"] * ops["aspect"], ops["spatscale_pix"]])
+        )
+        + 1
+    )
     diameter = diameter.flatten().astype(np.int64)
     Imed = signal.medfilt2d(I, [diameter[0], diameter[1]])
     I = I - Imed
@@ -43,12 +49,40 @@ def enhanced_mean_image(ops):
     mimg99 = 6
     mimg0 = I
 
-    mimg0 = mimg0[ops["yrange"][0]:ops["yrange"][1], ops["xrange"][0]:ops["xrange"][1]]
+    mimg0 = mimg0[
+        ops["yrange"][0] : ops["yrange"][1], ops["xrange"][0] : ops["xrange"][1]
+    ]
     mimg0 = (mimg0 - mimg1) / (mimg99 - mimg1)
     mimg0 = np.maximum(0, np.minimum(1, mimg0))
     mimg = mimg0.min() * np.ones((ops["Ly"], ops["Lx"]), np.float32)
-    mimg[ops["yrange"][0]:ops["yrange"][1], ops["xrange"][0]:ops["xrange"][1]] = mimg0
+    mimg[ops["yrange"][0] : ops["yrange"][1], ops["xrange"][0] : ops["xrange"][1]] = (
+        mimg0
+    )
     ops["meanImgE"] = mimg
     print("added enhanced mean image")
     return mimg
 
+
+def transform_mean_image_for_plotting(mean_image):
+    """Transforms the mean image for plotting
+
+    Parameters
+    ----------
+    mean_image : np.ndarray
+        Mean image to be transformed same from suite2p
+
+    Returns
+    -------
+    im0: NDArray[unsignedinteger[_8Bit]] | uint8 | NDArray | Any
+        Transformed mean image
+    """
+    im0 = mean_image.copy()
+    if im0.shape[0] < 4:
+        im0 = np.transpose(im0, (1, 2, 0))
+    if im0.shape[-1] < 3 or im0.ndim < 3:
+        im0 = plot.image_to_rgb(im0, channels=[0, 0])
+    else:
+        if im0.max() <= 50.0:
+            im0 = np.uint8(np.clip(im0, 0, 1) * 255)
+
+    return im0
