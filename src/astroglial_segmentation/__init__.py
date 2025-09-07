465a16e6ad23d661e70e26d383b399af87602ad3
javidaf <javid.af@outlook.com>
2025-09-07 12:56:38 +0200

    Add cellpose_segmentation import to the module

--- src/astroglial_segmentation/__init__.py
+++ src/astroglial_segmentation/__init__.py
@@ -1,4 +1,4 @@
-from .cellposesegmentation import segment_cells
+from .cellposesegmentation import segment_cells, cellpose_segmentation
 from .combination import combine_masks
 from .extraction import create_suite2p_masks_extract_traces
 from .utils import transform_mean_image_for_plotting
